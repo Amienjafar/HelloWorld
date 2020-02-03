2f89593b988bc8948f6cc17ef199413fed7749d5
Amienjafar <amienjafar120898@gmail.com>
2020-02-03 10:25:19 +0700

    initial commit

---
+++ app/src/main/java/org/d3ifcool/prak_4205_jurnal03_amienjafar/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.d3ifcool.prak_4205_jurnal03_amienjafar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
